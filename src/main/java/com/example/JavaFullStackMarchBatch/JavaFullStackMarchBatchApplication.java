67f56a9830182d28dfd2b716c5ab4489d87d104b
vaibhav <codekul.vaibhav@gmail.com>
2021-06-08 17:51:45 +0530

    DI and Scope

--- src/main/java/com/example/JavaFullStackMarchBatch/JavaFullStackMarchBatchApplication.java
+++ src/main/java/com/example/JavaFullStackMarchBatch/JavaFullStackMarchBatchApplication.java
@@ -1,13 +1,21 @@
 package com.example.JavaFullStackMarchBatch;
 
+import com.example.JavaFullStackMarchBatch.di.Company;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class JavaFullStackMarchBatchApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavaFullStackMarchBatchApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(JavaFullStackMarchBatchApplication.class, args);
+
+		Company company = context.getBean(Company.class);
+		company.showEmpInfo();
+
+		Company company1 = context.getBean(Company.class);
+		company1.showEmpInfo();
 	}
 
 }
---
+++ src/main/java/com/example/JavaFullStackMarchBatch/di/Company.java
@@ -0,0 +1,65 @@
+package com.example.JavaFullStackMarchBatch.di;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value = "prototype")
+public class Company {
+
+    int id;
+    String name;
+    String location;
+
+    public Company(){
+        System.out.println("In cont");
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    //field based injection
+//    @Autowired
+//    private Employee employee;
+
+    //constructor based injection
+    private Employee employee;
+
+//    @Autowired
+//    public Company(Employee employee){
+//        this.employee = employee;
+//    }
+
+//setter based injection
+    @Autowired
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+
+    public void showEmpInfo(){
+        employee.show();
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
